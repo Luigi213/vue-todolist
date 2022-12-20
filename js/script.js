6421498831128095aae7ba7284a890f9f82b9d50
Luigi213 <reyesgianluigi88@gmail.com>
2022-12-20 13:39:04 +0100

    Milestone 1 done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Descrizione: Rifare l'esercizio della to do list. Ogni todo sarà un oggetto, formato da due proprietà:
 // - text, una stringa che indica il testo del todo
 // - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-
+// MILESTONE 1
+// Stampare all'interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 const {
     createApp
 } = Vue;
@@ -9,7 +10,6 @@ const {
 createApp({
     data(){
         return {
-            listActive: 0,
             listObject: [
                 {
                     text: 'Fare colazione',
