e739419e44a193d2c26c8723351a82dad5349e57
Luigi213 <reyesgianluigi88@gmail.com>
2022-12-20 14:00:04 +0100

    Milestone 3 done

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@
 // Stampare all'interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 // MILESTONE 2
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 const {
     createApp
 } = Vue;
@@ -12,6 +14,7 @@ const {
 createApp({
     data(){
         return {
+            newObject: '',
             listObject: [
                 {
                     text: 'Fare colazione',
@@ -34,7 +37,14 @@ createApp({
     },
     methods: {
         deleteObject(index){
-            this.listObject.splice(index, 1)
+            this.listObject.splice(index, 1);
+        },
+        addObject(){
+            let obj = {
+                text: this.newObject,
+                done: false
+            };
+            this.listObject.push(obj);
         }
     },
 }).mount('#app')
